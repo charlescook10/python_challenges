80e58450b571c3d2d888883dfba6015f66f0c3c1
Charles Cook <charles.cook80@yahoo.co.uk>
2025-09-25 16:23:58 +0100

    chapter 3 program challenge done

--- python_chapter_3_challenges/program/lib/password_manager_2.py
+++ python_chapter_3_challenges/program/lib/password_manager_2.py
@@ -69,4 +69,43 @@
 
 from datetime import datetime
 class PasswordManager2():
-    pass
+    def __init__(self):
+        #   dictionary with dictionaries
+        #   {
+        #      service: {password: password, date_added: date},
+        #   }
+        self.storage = {}
+    def __is_valid_length(self, password):
+        return len(password) > 7
+    def __contains_special_char(self, password):
+        special_chars = "!@$%&"
+        return any(char in password for char in special_chars)
+    def __password_is_unique(self, password):
+        return list(filter(lambda d: d[1]["password"] == password, self.storage.items())) == []
+    def __is_valid(self, password):
+        return self.__is_valid_length(password) and self.__contains_special_char(password) and self.__password_is_unique(password)
+    def add(self, service, password):
+        if self.__is_valid(password):
+            self.storage[service]= {"password": password, "date_added": datetime.today()}
+        return None
+    def remove(self, service):
+        del self.storage[service]
+        return None
+    def update(self, service, password):
+        self.add(service, password)
+        return None
+    def list_services(self):
+        return list(self.storage.keys())
+    def get_for_service(self, service):
+        if service in self.storage:
+            return self.storage[service]["password"]
+        else: 
+            return None
+    def sort_services_by(self, sort, reverse_param=None):
+        #sort by service or by added_on date
+        reverse_bool = reverse_param == "reverse"
+        if sort == "service":
+            return sorted(self.list_services(), reverse=reverse_bool)
+        elif sort == "added_on":
+            sorted_dict = dict(sorted(self.storage.items(), key=lambda service: service[1]["date_added"], reverse=reverse_bool))
+            return list(sorted_dict.keys())
